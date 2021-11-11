4bacdf8e4b14e3e761b0624bbeaf8a056749713d
mhagg20 <matthias.hagg@online.de>
2021-11-11 09:33:18 +0100

    update

--- influxdb/foreground.sh
+++ influxdb/foreground.sh
@@ -1,5 +1,5 @@
 #/bin/bash
 
-echo This is a bash shell script.\n
-echo Any commands you put in here will be executed at runtime.\n
-echo As such, this is a good place to install and configure anything you want to have ready for the learner before they get started.\n
+echo \nThis is a bash shell script.\n\n
+echo \nAny commands you put in here will be executed at runtime.\n\n
+echo \nAs such, this is a good place to install and configure anything you want to have ready for the learner before they get started.\n\n
