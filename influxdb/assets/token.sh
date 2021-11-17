f97414e10c779e341d1950f13379afc544802129
mhagg20 <matthias.hagg@online.de>
2021-11-17 13:56:56 +0100

    Nachricht angepasst

--- influxdb/assets/token.sh
+++ influxdb/assets/token.sh
@@ -4,4 +4,9 @@ echo "Erstellung des Token wird durchgeführt!"
 influx auth create --org org --all-access > all_access_token.txt
 export INFLUX_TOKEN=$(grep -G -o -e [a-zA-Z0-9_.-]*== all_access_token.txt | head -n 2)
 
-echo "Erstellung des Token abgeschlossen!"
+if [ -z "$INFLUX_TOKEN" ]
+then
+      echo "Erstellung des Token fehlgeschlagen!"
+else
+      echo "Erstellung des Token abgeschlossen!"
+fi
